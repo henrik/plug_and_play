1d122f70271ae5047c8b3100ccc5d5c92266373e
Henrik Nyh <henrik@nyh.se>
2017-02-25 08:55:55 +0000

    Generated project.

---
+++ lib/plug_and_play.ex
@@ -0,0 +1,18 @@
+defmodule PlugAndPlay do
+  @moduledoc """
+  Documentation for PlugAndPlay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugAndPlay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_and_play_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugAndPlayTest do
+  use ExUnit.Case
+  doctest PlugAndPlay
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
